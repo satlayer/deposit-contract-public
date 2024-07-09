dcf14c0101926672556b17e5cc88f47e7ba12917
fman <feynyman@proton.me>
2024-07-09 18:22:35 +0200

    removed anvil private key from deploy script'

--- script/SatlayerPool.s.sol
+++ script/SatlayerPool.s.sol
@@ -9,7 +9,6 @@ contract DeploySatlayerPoolSepolia is Script {
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        //uint256 deployerPrivateKey = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
         uint256 DECIMALS = 10**18;
 
         vm.startBroadcast(deployerPrivateKey);
